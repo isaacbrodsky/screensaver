09b79df69a32268d0a064e571ef334c2179af700
Isaac Brodsky <isaac@isaacbrodsky.com>
2017-11-12 09:53:22 -0800

    stars

--- include/main.h
+++ include/main.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <random>
+#include <list>
 #include "SDL.h"
 
 class Building {
 public: // TODO
+	int offsetX;
+	int offsetY;
+
 	std::mt19937& rand;
 	int w, h;
 	int windowWidth;
@@ -12,10 +16,12 @@ public: // TODO
 	int windowOffset;
 
 public:
-	Building(std::mt19937& rand, int w, int h, int windowWidth, int windowHeight, int windowOffset);
+	Building(std::mt19937& rand, int offsetX, int offsetY, int w, int h, int windowWidth, int windowHeight, int windowOffset);
 
 	void Update();
-	void Draw(SDL_Renderer *ren, int offsetX, int screenHeight);
+	void Draw(SDL_Renderer *ren) const;
+
+	SDL_Rect ToRect() const;
 };
 
 class Scrsvr_State {
@@ -25,17 +31,16 @@ private:
 
 	std::mt19937 rand;
 
-	// Current drawing state
-	int i;
-
 	// State to draw
 	int numBuildings;
 	std::vector<Building> buildings;
+	int maxStars;
+	std::list<SDL_Point> stars;
 
 public:
 	Scrsvr_State(SDL_Renderer *ren, int w, int h, int scaling);
 	~Scrsvr_State();
 
 	void Update();
-	void Draw(SDL_Renderer *ren);
+	void Draw(SDL_Renderer *ren) const;
 };
--- src/main.cpp
+++ src/main.cpp
@@ -21,21 +21,34 @@ int max(int a, int b) {
 	}
 }
 
-Building::Building(std::mt19937& rand, int w, int h, int ww, int wh, int wo)
-	: rand(rand), w(w), h(h), windowWidth(ww), windowHeight(wh), windowOffset(wo) {
+Building::Building(std::mt19937& rand, int ox, int oy, int w, int h, int ww, int wh, int wo)
+	: rand(rand), offsetX(ox), offsetY(oy), w(w), h(h), windowWidth(ww), windowHeight(wh), windowOffset(wo) {
 
+}
+
+SDL_Rect Building::ToRect() const {
+	SDL_Rect outline;
+	outline.x = offsetX;
+	outline.y = offsetY;
+	outline.w = w;
+	outline.h = h;
+	return outline;
 }
 
 void Building::Update() {
 
 }
 
-void Building::Draw(SDL_Renderer *ren, int offsetX, int screenHeight) {
+void Building::Draw(SDL_Renderer *ren) const {
+	SDL_Rect outline = ToRect();
+
+	// Clear
+	SDL_SetRenderDrawColor(ren, 0, 0, 0, 0);
+	SDL_RenderFillRect(ren, &outline);
+
 	// Draw building outlines
 	SDL_SetRenderDrawColor(ren, 0, 255, 0, 0);
-	SDL_RenderDrawLine(ren, offsetX, screenHeight, offsetX, screenHeight - h);
-	SDL_RenderDrawLine(ren, offsetX, screenHeight - h, offsetX + w, screenHeight - h);
-	SDL_RenderDrawLine(ren, offsetX + w, screenHeight, offsetX + w, screenHeight - h);
+	SDL_RenderDrawRect(ren, &outline);
 
 	// Draw window outlines
 	SDL_SetRenderDrawColor(ren, 0, 255, 255, 0);
@@ -49,20 +62,26 @@ void Building::Draw(SDL_Renderer *ren, int offsetX, int screenHeight) {
 			rect.h = windowHeight;
 			rect.w = windowWidth + 1;
 			rect.x = offsetX + k;
-			rect.y = screenHeight - j;
+			rect.y = offsetY + j;
 			SDL_RenderDrawRect(ren, &rect);
 		}
 	}
 }
 
 Scrsvr_State::Scrsvr_State(SDL_Renderer *ren, int w, int h, int scaling) : w(w / scaling), h(h / scaling), rand(std::chrono::system_clock::now().time_since_epoch().count()) {
-	i = 0;
 	tex = SDL_CreateTexture(ren, SDL_PIXELFORMAT_RGB888, SDL_TextureAccess::SDL_TEXTUREACCESS_TARGET, w / scaling, h / scaling);
 
+	maxStars = rand() % 100 + 100;
+
 	numBuildings = rand() % 20 + 5;
+	int offsetX = 0;
 	for (int i = 0; i < numBuildings; i++) {
-		Building building(rand, rand() % 70 + 60, rand() % h, max(rand() % 5, 1), max(rand() % 5, 1), rand() % 10 + 1);
+		int bw = rand() % 70 + 60;
+		int bh = rand() % (h / scaling);
+		int offsetY = (h / scaling) - bh;
+		Building building(rand, offsetX, offsetY, bw, bh, max(rand() % 5, 1), max(rand() % 5, 1), rand() % 10 + 1);
 		buildings.push_back(building);
+		offsetX += bw;
 	}
 }
 
@@ -71,28 +90,38 @@ Scrsvr_State::~Scrsvr_State() {
 }
 
 void Scrsvr_State::Update() {
-	i++;
-	if (i >= numBuildings) {
-		i = 0;
+	bool newStarValid = true;
+	SDL_Point newStar = { rand() % w, rand() % h };
+
+	for (Building& building : buildings) {
+		building.Update();
+
+		SDL_Rect rect = building.ToRect();
+		if (SDL_PointInRect(&newStar, &rect)) {
+			newStarValid = false;
+		}
 	}
 
-	buildings[i].Update();
+	if (newStarValid) {
+		stars.push_front(newStar);
+
+		if (stars.size() > maxStars) {
+			stars.pop_back();
+		}
+	}
 }
 
-void Scrsvr_State::Draw(SDL_Renderer *ren) {
+void Scrsvr_State::Draw(SDL_Renderer *ren) const {
 	SDL_SetRenderTarget(ren, tex);
 
-	if (i == 0) {
-		SDL_SetRenderDrawColor(ren, 0, 0, 0, 0);
-		SDL_RenderClear(ren);
+	for (const Building& building : buildings) {
+		building.Draw(ren);
 	}
 
-	int offset = 0;
-	for (int j = 0; j < i; j++) {
-		offset += buildings[j].w;
+	SDL_SetRenderDrawColor(ren, 255, 255, 255, 0);
+	for (const SDL_Point& star : stars) {
+		SDL_RenderDrawPoint(ren, star.x, star.y);
 	}
-
-	buildings[i].Draw(ren, offset, h);
 
 	SDL_SetRenderTarget(ren, NULL);
 	SDL_RenderCopy(ren, tex, NULL, NULL);
